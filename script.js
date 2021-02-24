7a2582d49e1d6c9b92e95a780fc9988641033af4
lyusk8 <lyusk8@gmail.com>
2021-02-24 11:24:55 -0300

    criado cobrinha e movimento

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
 const canvas = document.getElementById('snake');
 const context = canvas.getContext('2d');
-const box = 32;
+let cubo = 32;
+let cobra = [];
+cobra[0] = {
+  x: 8 * cubo,
+  y: 8 * cubo,
+};
+let direcao = 'direita';
 
 function criarFundo() {
   context.fillStyle = 'lightgreen';
-  context.fillRect(0, 0, canvas.width, canvas.width);
+  context.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-criarFundo();
+function criarCobra() {
+  context.fillStyle = 'green';
+  for (let i = 0; i < cobra.length; i++) {
+    context.fillRect(cobra[i].x, cobra[i].y, cubo, cubo);
+  }
+}
+
+function jogar() {
+  criarFundo();
+  criarCobra();
+  let cobrax = cobra[0].x;
+  let cobray = cobra[0].y;
+  if (direcao === 'direita') cobrax += cubo;
+  if (direcao === 'esquerda') cobrax -= cubo;
+  if (direcao === 'acima') cobrax -= cubo;
+  if (direcao === 'abaixo') cobrax += cubo;
+
+  cobra.pop();
+
+  let cabeca = {
+    x: cobrax,
+    y: cobray,
+  };
+
+  cobra.unshift(cabeca);
+}
+
+let jogo = setInterval(jogar, 100);
